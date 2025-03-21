b5737007e494c831d37d23714edc715381f84812
drbestie <educationmail06@gmail.com>
2025-03-21 07:58:52 +0530

    Initial commit

---
+++ vars.py
@@ -0,0 +1,11 @@
+# Sanjay Kagra 
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+
+API_ID    = os.environ.get("API_ID", "28996064")
+API_HASH  = os.environ.get("API_HASH", "e09920a63e5f157b85a64ddba66596c6")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8155433245:AAGVNb9qUsysU06AO2TWJVdWDjmIc7ESUJI") 
+
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
